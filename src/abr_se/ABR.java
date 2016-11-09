22db03b15964a0c72b2750a9e5f3048b9f19ce99
MarjorieJ <marjorie.julio@depinfonancy.net>
2016-11-09 14:39:29 +0100

    Creation des premieres methodes

--- src/abr_se/ABR.java
+++ src/abr_se/ABR.java
@@ -1,5 +1,43 @@
 package abr_se;
 
-public class ABR {
+import java.util.List;
 
+public class ABR {
+	private int nbElements;
+	private Noeud root;
+	private List<Noeud> tree;
+	
+	public ABR(Noeud n, int nbElem){	
+		this.nbElements=nbElem;
+		this.root=n;
+		tree.add(this.root);
+	}
+	
+	public ABR(){
+		this.nbElements=0;
+		this.root=null;
+	}
+	
+	public int nbElements(){
+		return this.nbElements;
+	}
+	
+	public boolean isEmpty(){
+		return (this.nbElements==0);
+	}
+	
+	public void insert(Noeud n){
+
+		if (this.isEmpty()){
+			this.root=n;
+			return;
+		}
+		//n.addParent(root);
+		//while{root.hasChildren()
+			
+		//}
+		
+	}
+	
+	
 }
